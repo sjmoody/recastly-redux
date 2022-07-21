5bb3d70d777355282e53163bf80e4f1d827f8406
Cameron Hirsh <camjhirsh@gmail.com>
2022-07-20 20:52:13 -0700

    pass up to root reducer

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,20 +1,10 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
+var currentVideoReducer = (state = null, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
-  console.log('a', action);
-  console.log('state', state);
-
-  if (!state) {
-    state = {
-      videoList: [],
-      currentVideo: null
-    };
-  }
 
   if (action.type === 'CHANGE_VIDEO') {
-    const newState = [...state, action.video];
-    return newState;
+    return action.video;
   } else {
     return state;
   }
--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -3,26 +3,17 @@ import exampleVideoData from '../data/exampleVideoData.js';
 
 //
 
-var videoListReducer = (state = exampleVideoData, action) => {
+var videoListReducer = (state = [], action) => {
   //TODO: define a reducer for the videoList field of our state.
 
   console.log('a', action);
   console.log('state', state);
 
-  if (action && action.type === 'CHANGE_VIDEO_LIST') {
-    newState.videos = action.videos;
-    currentVideo = action.videos[0];
-    const newState = {
-      videos: action.videos,
-      value: state.value,
-      currentVideo: action.videos[0]
-    };
-
+  if (action.type === 'CHANGE_VIDEO_LIST') {
+    return action.videos;
   } else {
     return state;
   }
-
-  return newState;
 
 };
 
